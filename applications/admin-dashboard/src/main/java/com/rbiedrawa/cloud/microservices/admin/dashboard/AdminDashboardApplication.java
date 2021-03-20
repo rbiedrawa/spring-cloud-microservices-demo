ee4e7cf50ad73572915b57e758280bbbb322c57e
rbiedrawa <biedrawa.rafal@gmail.com>
2021-03-20 16:43:36 +0100

    Added hystrix dashboard to spring boot admin

--- applications/admin-dashboard/src/main/java/com/rbiedrawa/cloud/microservices/admin/dashboard/AdminDashboardApplication.java
+++ applications/admin-dashboard/src/main/java/com/rbiedrawa/cloud/microservices/admin/dashboard/AdminDashboardApplication.java
@@ -2,7 +2,6 @@ package com.rbiedrawa.cloud.microservices.admin.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
